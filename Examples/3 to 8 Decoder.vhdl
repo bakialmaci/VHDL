66181cf0599a5ea2458ca9548ed46fc367809f94
Kaan Arı <kaanari@hacettepe.edu.tr>
2020-10-01 16:10:03 +0300

    RTL Architecture is added.
    Alternative Syntax is added and tested by using ModelSim-Altera.

--- Examples/3 to 8 Decoder.vhdl
+++ Examples/3 to 8 Decoder.vhdl
@@ -21,3 +21,31 @@ d(6) <= (x) and (y) and (not z);
 d(7) <= (x) and (y) and (z);
 end Behavioral;
 
+architecture RTL of three_eight_decoder is
+
+signal combined : unsigned(2 downto 0) := "000";
+
+begin
+    
+	 combined <= x & y & z;
+	 
+	 process(combined) 
+	 begin
+	 
+	 case to_integer(combined) is
+	 
+		when 0 => d <= (0 => '1', others => '0');
+		when 1 => d <= (1 => '1', others => '0');
+		when 2 => d <= (2 => '1', others => '0');
+		when 3 => d <= (3 => '1', others => '0');
+		when 4 => d <= (4 => '1', others => '0');
+		when 5 => d <= (5 => '1', others => '0');
+		when 6 => d <= (6 => '1', others => '0');
+		when 7 => d <= (7 => '1', others => '0');
+		when others => d <= (others => '0');
+		
+	 end case;
+	
+	 end process;
+	 
+end RTL;
